8fcb9955d1fb0a87b7a88c4c30c0c50832b8c989
vicverevita <2victor.gabriel01@gmail.com>
2023-06-13 15:20:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResults" :score="score"/>
+  <!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+export default {
+  name: 'App',
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+
+</style>
